ad38982ed8ad9f467fee79479d6f3658aeaab618
rafaelfernandezgis <rafael.fernandez.gis@gmail.com>
2018-10-10 17:12:45 +0200

    adding build files

---
+++ build/precache-manifest.796c39d169a8a00a006b8c79412d5c2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "275d41dd7503ed761c19",
+    "url": "/static/js/main.275d41dd.chunk.js"
+  },
+  {
+    "revision": "8712b664f688500b982d",
+    "url": "/static/js/1.8712b664.chunk.js"
+  },
+  {
+    "revision": "275d41dd7503ed761c19",
+    "url": "/static/css/main.85488ac7.chunk.css"
+  },
+  {
+    "revision": "8712b664f688500b982d",
+    "url": "/static/css/1.9f39084b.chunk.css"
+  },
+  {
+    "revision": "016159a9b645224fa4c8446fae83e897",
+    "url": "/index.html"
+  }
+];
